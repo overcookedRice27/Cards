3c1f63abe258657a1495b22a05907898f2c92807
Tom Lu <_tom_lu_@Toms-MacBook-Air.local>
2025-04-02 13:02:41 -0700

    added a WIP tester for the classes

---
+++ CardTester.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.Assert;
+public class CardTester {
+
+    @Test 
+    public 
+    //size tests
+    CardDeck testDeck = new CardDeck();
+    assertEquals(52,testDeck.getDeckSize());
+    
+    
+}
